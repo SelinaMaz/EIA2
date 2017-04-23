57bc03f98d15253037e264da62e4479ae83ca42c
Fuma <Fuma@192.168.2.103>
2017-04-23 20:37:11 +0200

    Update

--- aufgabe_03/js/a03a.js
+++ aufgabe_03/js/a03a.js
@@ -10,87 +10,96 @@ In Zusammenarbeit mit Jana Burger und Jacqueliene Wagner.
 Leider ist diese Aufgabe nicht vollst�ndig aufgrund fehlender Verst�ndis meinerseits.
 Ich hab es auch nicht geschafft die Aufgabe in der Gruppe zul�sen und w�re deshalb sehr dankbar,
 wenn wir diese Aufgabe im Praltikum nochmal Besprechen k�nnten.
-
-Ich versuche es bis dahin weiter zu l�sen...
 */
-let zeile = 0;
-let a = 1; //Anzahl Reis
-document.addEventListener("DOMContentLoaded", function () {
-    //Aussehen von Body bleibt immer gleich 
-    let b = document.body.style;
-    b.width = "450px";
-    b.margin = "50px";
-    b.backgroundColor = "#F7F7F7";
-    let countField = 0;
-    //Schachbrett
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            Div();
-            if (i % 2 == j % 2) {
-                white(countField);
+var A3;
+(function (A3) {
+    let zeile = 0;
+    let a = 1; //Anzahl Reis
+    document.addEventListener("DOMContentLoaded", function () {
+        //Aussehen von Body bleibt immer gleich 
+        let b = document.body.style;
+        b.width = "450px";
+        b.margin = "50px";
+        b.backgroundColor = "#F7F7F7";
+        let countField = 0;
+        //Schachbrett
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                Div();
+                if (i % 2 == j % 2) {
+                    white(countField);
+                }
+                else {
+                    black(countField);
+                }
+                countField++;
             }
-            else {
-                black(countField);
-            }
-            countField++;
+        }
+        //Aufgabe 3a
+        for (let i = 0; i < 8; i++) {
+            var total = 0;
+            console.log("Schleife");
+            let fields = document.getElementsByTagName("div");
+            fields[i].addEventListener("click", function () {
+                fields[i].classList.toggle("selected");
+                console.log(fields[i]);
+                let selected = document.getElementsByClassName("selected");
+                console.log(selected);
+                if (selected.length == 0) {
+                    document.getElementById("inhalt").style.display = "none";
+                }
+                else {
+                    document.getElementById("inhalt").style.display = "inline-block";
+                }
+                document.getElementById("inhalt").textContent = "Summe der K�rner - " + "Dezimal: " + total.toString() + ";" + " Hexadezimal: " + total.toString(16);
+                for (let i = 0; i < selected.length; i++) {
+                    total += Number(selected[i].textContent);
+                }
+            });
+        }
+        document.addEventListener("mousemove", function (Event) {
+            document.getElementById("inhalt").style.left = (Event.clientX + 10) + "px";
+            document.getElementById("inhalt").style.top = (Event.clientY + 10) + "px";
+        });
+    });
+    //Divs erzeugen
+    function Div() {
+        let div = document.createElement("div");
+        document.body.appendChild(div);
+        div.className = "DivBox";
+        //Aussehen von Divs
+        let s = div.style;
+        s.borderStyle = "solid";
+        s.borderColor = "#000000";
+        s.borderWidth = "1px";
+        s.width = "50px";
+        s.height = "50px";
+        s.display = "inline-block";
+        s.cssFloat = "left";
+        s.overflow = "auto";
+        //Reis
+        div.innerText = "" + a;
+        a = a * 2;
+    }
+    //Schwarze Divs
+    function black(_number) {
+        let div = document.getElementsByTagName("div");
+        let currentDiv = div[_number];
+        if (div.length != 0) {
+            currentDiv.style.backgroundColor = "black";
+            currentDiv.style.color = "white";
+            currentDiv.style.textAlign = "center";
         }
     }
-    /********************************************************************************************************
-   Aufgabe 3a
-   ********************************************************************************************************/
-    let x = 0;
-    let divList = document.getElementsByTagName("div");
-    for (x; x < 8; x++) {
-        divList[x].addEventListener("click", clickEvent);
+    //Wei�e Divs
+    function white(_number) {
+        let div = document.getElementsByTagName("div");
+        let currentDiv = div[_number];
+        if (div.length != 0) {
+            currentDiv.style.backgroundColor = "white";
+            currentDiv.style.color = "black";
+            currentDiv.style.textAlign = "center";
+        }
     }
-});
-//Divs erzeugen
-function Div() {
-    let div = document.createElement("div");
-    document.body.appendChild(div);
-    div.className = "DivBox";
-    //Aussehen von Divs
-    let s = div.style;
-    s.borderStyle = "solid";
-    s.borderColor = "#000000";
-    s.borderWidth = "1px";
-    s.width = "50px";
-    s.height = "50px";
-    s.display = "inline-block";
-    s.cssFloat = "left";
-    s.overflow = "auto";
-    //Reis
-    div.innerText = "" + a;
-    a = a * 2;
-}
-//Schwarze Divs
-function black(_number) {
-    let div = document.getElementsByTagName("div");
-    let currentDiv = div[_number];
-    if (div.length != 0) {
-        currentDiv.style.backgroundColor = "black";
-        currentDiv.style.color = "white";
-        currentDiv.style.textAlign = "center";
-    }
-}
-//Wei�e Divs
-function white(_number) {
-    let div = document.getElementsByTagName("div");
-    let currentDiv = div[_number];
-    if (div.length != 0) {
-        currentDiv.style.backgroundColor = "white";
-        currentDiv.style.color = "black";
-        currentDiv.style.textAlign = "center";
-    }
-}
-/********************************************************************************************************
-Aufgabe 3a
-********************************************************************************************************/
-let state = [false, false, false, false, false, false, false, false];
-let div = document.getElementsByTagName("div");
-function clickEvent(_event) {
-    let target = _event.target;
-    for (let i = 0; i < state.length; i++) {
-    }
-}
+})(A3 || (A3 = {}));
 //# sourceMappingURL=a03a.js.map
--- aufgabe_03/js/a03a.ts
+++ aufgabe_03/js/a03a.ts
@@ -10,103 +10,115 @@ In Zusammenarbeit mit Jana Burger und Jacqueliene Wagner.
 Leider ist diese Aufgabe nicht vollständig aufgrund fehlender Verständis meinerseits. 
 Ich hab es auch nicht geschafft die Aufgabe in der Gruppe zulösen und wäre deshalb sehr dankbar, 
 wenn wir diese Aufgabe im Praltikum nochmal Besprechen könnten.
-
-Ich versuche es bis dahin weiter zu lösen...
 */
+namespace A3 {
+    let zeile: number = 0;
+    let a: number = 1; //Anzahl Reis
 
-let zeile: number = 0;
-let a: number = 1; //Anzahl Reis
+    document.addEventListener("DOMContentLoaded", function(): void {
 
-document.addEventListener("DOMContentLoaded", function(): void {
+        //Aussehen von Body bleibt immer gleich 
+        let b: CSSStyleDeclaration = document.body.style;
+        b.width = "450px";
+        b.margin = "50px";
+        b.backgroundColor = "#F7F7F7";
 
-    //Aussehen von Body bleibt immer gleich 
-    let b: CSSStyleDeclaration = document.body.style;
-    b.width = "450px";
-    b.margin = "50px";
-    b.backgroundColor = "#F7F7F7";
+        let countField: number = 0;
 
-    let countField: number = 0;
-
-    //Schachbrett
-    for (let i: number = 0; i < 8; i++) {
-        for (let j: number = 0; j < 8; j++) {
-            Div();
-            if (i % 2 == j % 2) {
-                white(countField);
+        //Schachbrett
+        for (let i: number = 0; i < 8; i++) {
+            for (let j: number = 0; j < 8; j++) {
+                Div();
+                if (i % 2 == j % 2) {
+                    white(countField);
+                }
+                else {
+                    black(countField);
+                }
+                countField++;
             }
-            else {
-                black(countField);
-            }
-            countField++;
+        }
+
+        //Aufgabe 3a
+
+        for (let i: number = 0; i < 8; i++) {
+            var total: number = 0;
+
+            console.log("Schleife");
+            let fields: NodeListOf<HTMLElement> = document.getElementsByTagName("div");
+            fields[i].addEventListener("click", function() {
+                fields[i].classList.toggle("selected");
+
+                console.log(fields[i]);
+
+                let selected: NodeListOf<Element> = document.getElementsByClassName("selected");
+
+                console.log(selected);
+
+                if (selected.length == 0) {
+                    document.getElementById("inhalt").style.display = "none";
+                }
+
+                else {
+                    document.getElementById("inhalt").style.display = "inline-block";
+                }
+
+                document.getElementById("inhalt").textContent = "Summe der Körner - " + "Dezimal: " + total.toString() + ";" + " Hexadezimal: " + total.toString(16);
+
+
+                for (let i: number = 0; i < selected.length; i++) {
+                    total += Number(selected[i].textContent);
+                }
+            });
+        }
+        document.addEventListener("mousemove", function(Event) {
+            document.getElementById("inhalt").style.left = (Event.clientX + 10) + "px";
+            document.getElementById("inhalt").style.top = (Event.clientY + 10) + "px";
+        });
+
+    });
+
+    //Divs erzeugen
+    function Div(): void {
+        let div: HTMLDivElement = document.createElement("div");
+        document.body.appendChild(div);
+        div.className = "DivBox";
+
+        //Aussehen von Divs
+        let s: CSSStyleDeclaration = div.style;
+        s.borderStyle = "solid";
+        s.borderColor = "#000000";
+        s.borderWidth = "1px";
+        s.width = "50px";
+        s.height = "50px";
+        s.display = "inline-block";
+        s.cssFloat = "left";
+        s.overflow = "auto";
+
+        //Reis
+        div.innerText = "" + a;
+        a = a * 2;
+    }
+
+    //Schwarze Divs
+    function black(_number: number): void {
+        let div: NodeList = document.getElementsByTagName("div");
+        let currentDiv: HTMLDivElement = <HTMLDivElement>div[_number];
+        if (div.length != 0) {
+            currentDiv.style.backgroundColor = "black";
+            currentDiv.style.color = "white";
+            currentDiv.style.textAlign = "center";
         }
     }
-
-    /********************************************************************************************************
-   Aufgabe 3a
-   ********************************************************************************************************/
-    let x: number = 0;
-    let divList: NodeListOf<HTMLElement> = document.getElementsByTagName("div");
-
-    for (x; x < 8; x++) {
-        divList[x].addEventListener("click", clickEvent);
-    }
-
-});
-
-//Divs erzeugen
-function Div(): void {
-    let div: HTMLDivElement = document.createElement("div");
-    document.body.appendChild(div);
-    div.className = "DivBox";
-
-    //Aussehen von Divs
-    let s: CSSStyleDeclaration = div.style;
-    s.borderStyle = "solid";
-    s.borderColor = "#000000";
-    s.borderWidth = "1px";
-    s.width = "50px";
-    s.height = "50px";
-    s.display = "inline-block";
-    s.cssFloat = "left";
-    s.overflow = "auto";
-
-    //Reis
-    div.innerText = "" + a;
-    a = a * 2;
-}
-
-//Schwarze Divs
-function black(_number: number): void {
-    let div: NodeList = document.getElementsByTagName("div");
-    let currentDiv: HTMLDivElement = <HTMLDivElement>div[_number];
-    if (div.length != 0) {
-        currentDiv.style.backgroundColor = "black";
-        currentDiv.style.color = "white";
-        currentDiv.style.textAlign = "center";
-    }
-}
-//Weiße Divs
-function white(_number: number): void {
-    let div: NodeList = document.getElementsByTagName("div");
-    let currentDiv: HTMLDivElement = <HTMLDivElement>div[_number];
-    if (div.length != 0) {
-        currentDiv.style.backgroundColor = "white";
-        currentDiv.style.color = "black";
-        currentDiv.style.textAlign = "center";
-    }
-}
-
-/********************************************************************************************************
-Aufgabe 3a
-********************************************************************************************************/
-let state: boolean[] = [false, false, false, false, false, false, false, false];
-let div: NodeList = document.getElementsByTagName("div");
-
-function clickEvent(_event: MouseEvent): void {
-    let target: HTMLElement = <HTMLElement>_event.target;
-
-    for (let i: number = 0; i < state.length; i++) {
-        
+    //Weiße Divs
+    function white(_number: number): void {
+        let div: NodeList = document.getElementsByTagName("div");
+        let currentDiv: HTMLDivElement = <HTMLDivElement>div[_number];
+        if (div.length != 0) {
+            currentDiv.style.backgroundColor = "white";
+            currentDiv.style.color = "black";
+            currentDiv.style.textAlign = "center";
+        }
     }
 
 
