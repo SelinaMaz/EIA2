dab9c08d5734ee4e8c4609c228d61b9faf8ec0b2
Fuma <Fuma@141.28.227.5>
2017-04-24 23:28:37 +0200

    Portfolio

---
+++ Portfolio/js/meinPortfolio.ts
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //Klappmen�
+    var burgerNav = document.getElementById("BurgerNav");
+    var burgerButton = document.getElementById("burger");
+    var state = true;
+    burgerButton.addEventListener("click", function () {
+        if (state) {
+            burgerNav.style.display = "block";
+            state = false;
+        }
+        else {
+            burgerNav.style.display = "none";
+            state = true;
+        }
+    });
+    //Alle h2 grauer Hintergrund und wei�e Schrift
+    var elems = document.getElementsByTagName("h2");
+    for (var i = 0; i < elems.length; i++) {
+        var Unterueberschrift = elems[i].getElementsByClassName("Unterueberschrift");
+        console.log(i);
+        console.log(elems);
+        if (elems.length != 0) {
+            elems[i].style.backgroundColor = "#9B9B9B";
+            elems[i].style.color = "white";
+        }
+    }
+});
