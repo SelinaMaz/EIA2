219291f7f78f4a267fe8015a068464ea4603c0ff
Fuma <Fuma@141.28.227.5>
2017-04-25 03:08:38 +0200

    Portfolio

--- Portfolio/js/meinPortfolio.js
+++ Portfolio/js/meinPortfolio.js
@@ -13,16 +13,5 @@ document.addEventListener('DOMContentLoaded', function () {
             state = true;
         }
     });
-    //Alle h2 grauer Hintergrund und wei�e Schrift
-    var elems = document.getElementsByTagName("h2");
-    for (var i = 0; i < elems.length; i++) {
-        var Unterueberschrift = elems[i].getElementsByClassName("Unterueberschrift");
-        console.log(i);
-        console.log(elems);
-        if (elems.length != 0) {
-            elems[i].style.backgroundColor = "#9B9B9B";
-            elems[i].style.color = "white";
-        }
-    }
 });
 //# sourceMappingURL=meinPortfolio.js.map
