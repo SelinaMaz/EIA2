39141e4480e323d2aead9a37ae764d40995b972c
Fuma <Fuma@141.28.134.48>
2017-04-26 15:02:56 +0200

    Portfolio

---
+++ Portfolio/js/portfolio.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //Klappmen�
+    var burgerNav = document.getElementById("BurgerNav");
+    var burgerButton = document.getElementById("burger");
+    var state = true;
+    burgerButton.addEventListener("click", function () {
+        if (state) {
+            burgerNav.style.display = "block";
+            state = false;
+        }
+        else {
+            burgerNav.style.display = "none";
+            state = true;
+        }
+    });
+});
+//# sourceMappingURL=portfolio.js.map
---
+++ Portfolio/js/portfolio.ts
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //Klappmen�
+    var burgerNav = document.getElementById("BurgerNav");
+    var burgerButton = document.getElementById("burger");
+    var state = true;
+    burgerButton.addEventListener("click", function () {
+        if (state) {
+            burgerNav.style.display = "block";
+            state = false;
+        }
+        else {
+            burgerNav.style.display = "none";
+            state = true;
+        }
+    });
+});
